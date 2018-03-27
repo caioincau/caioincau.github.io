35c2d4acd3cce5f619e2ebb471875414a7f2bec5
Caio Incau <caioincau@gmail.com>
2018-03-27 10:13:46 -0300

    service worker

---
+++ _site/sw.js
@@ -0,0 +1,45 @@
+const staticCacheName = 'caio-incau-2018-03-27-10-13';
+
+const filesToCache = [
+  
+  
+    '/porque-decidi-escrever/',
+  
+  
+];
+
+// Cache on install
+this.addEventListener("install", event => {
+  this.skipWaiting();
+
+  event.waitUntil(
+    caches.open(staticCacheName)
+      .then(cache => {
+        return cache.addAll(filesToCache);
+    })
+  )
+});
+
+// Clear cache on activate
+this.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames
+          .filter(cacheName => (cacheName.startsWith('caio-incau-')))
+          .filter(cacheName => (cacheName !== staticCacheName))
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+// Serve from Cache
+this.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  )
+});
